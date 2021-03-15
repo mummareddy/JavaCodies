d24590da5b30819baf942d6d41a7a65b90c37ad8
mahesh.mummareddy@hotstar.com <mummareddym@Admins-MacBook-Pro-16.local>
2021-03-15 15:16:55 +0530

    add 2 nums

---
+++ src/main/java/com/mahesh/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.mahesh;
+
+/**
+ * Created by mummareddym on 15/03/2021
+ */
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a=10, b=5;
+        System.out.println(add(a, b));
+        System.out.println(add(a, b));
+    }
+    static int add(int a, int b){
+        return a+b;
+    }
+}
